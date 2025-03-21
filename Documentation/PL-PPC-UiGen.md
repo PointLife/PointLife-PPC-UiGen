997bf38789caa1ed0b068b6782973eebc903df96
PLASMAchicken <plasmachickenmc@gmail.com>
2025-03-22 00:02:14 +0100

    Projektdateien hinzufügen.

---
+++ Documentation/PL-PPC-UiGen.md
@@ -0,0 +1,74 @@
+1. Import unitypackage
+	![[PL_PPC_UiGen.unitypackage]]
+2. Wait for Unity to Recompile
+3. .uxml files need to be in Format [PluginName]\_[Category/Folder]\_[MenuName], eg `BetterBP_Estate_ManageSettings`
+4. UI Objects Name needs to be set to the Variable Name
+	![[Pasted image 20250321232252.png]]
+5. If you want a Close Button it needs to be called "CloseMenu"
+	![[Pasted image 20250321232407.png]]
+6. Open PointLife-PPC-UiGen -> Generate UI Code
+	![[Pasted image 20250321231514.png]]
+	![[Pasted image 20250321232517.png]]
+7. Select the Export Path ( A Folder in your Visual Studio Solution for your Plugin )
+8. Enable DISABLE TEXT to show the names of the Fields
+9. Accepted ( or change ) the suggested Changes to fix the Issues
+	![[Pasted image 20250321232904.png]]
+10. Press Build and Export Files
+11. Add 0PPC-UiGen-BaseType.dll Refrence to your Plugin and into the Plugins Folder or Managed Folder
+	![[0PPC-UiGen-BaseType.dll]]
+```cs
+<Reference Include="0PPC-UiGen-BaseType">
+	<HintPath>$(BPDIR)\Plugins\0PPC-UiGen-BaseType.dll</HintPath>
+</Reference>
+```
+12. If using SDK-type .csproj the exported Folder will be automatically incldued.
+13. Make a new Class and add `using PointLife.UiGen.Scaffold;` and inherit from `scaffold_`
+	![[Pasted image 20250321233648.png]]
+14. Use Visual Studio Alt+Enter to Generate the Class
+    ![[Pasted image 20250321234033.png]]
+	Choose to override Open and Close ( if needed )
+	![[Pasted image 20250321234313.png]]
+
+You can now add Code
+ ```cs
+using BPEssentials.ExtensionMethods;
+using BrokeProtocol.Entities;
+using PointLife.UiGen.Scaffold;
+
+namespace BPPlasmaGangs.Menus.UiToolkit.Menus
+{
+	public class ManageSettings : scaffold_BetterBP_Estate_ManageSettings
+	{
+		public ManageSettings(ShPlayer _player) : base(_player)
+		{
+		}
+
+		public override void Open()
+		{
+			Fields.Text_Password.UpdateTo("123456789");     // Write last Password
+			Fields.Toggle_AllowVisitors.UpdateTo(true);     // Write last Toggle
+
+			base.Open();
+		}
+
+		public override void On_Save_Pressed()
+		{
+			Fields.UpdateAll(player).Then(() =>             // Get all Fields from the Client
+			{
+				player.TS("settings_saved");
+
+				player.TS("setting_toggle", Fields.Toggle_AllowVisitors.CheckboxValue);
+				player.TS("setting_password", Fields.Text_Password.Text);
+
+				Close();    // Close Menu
+			});
+		}
+	}
+}
+
+```
+
+In a Command it can then be used like:
+```cs
+new ManageSettings(player).Open();
+```
---
+++ PointLife-PPC-UiGen/BaseTypes.cs
@@ -0,0 +1,144 @@
+﻿using BrokeProtocol.Entities;
+using RSG;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace PointLife.UiGen.BaseFields
+{
+    public class BaseMenuFields
+    {
+        public List<BaseField> Fields = new List<BaseField>();
+
+        public virtual IPromise UpdateAll(ShPlayer player)
+        {
+            return Promise.All(Fields.Select(x => x.GetValue()));
+        }
+    }
+
+    public abstract class BaseField(string name, ShPlayer player)
+    {
+        public string Name { get; internal set; } = name;
+        public ShPlayer Player { get; internal set; } = player;
+
+        internal Promise? Promise;
+
+        public virtual Promise GetValue()
+        {
+            if (Promise == null || Promise.CurState == PromiseState.Resolved)
+            {
+                return Promise = new Promise();
+            }
+            else
+            {
+                return Promise;
+            }
+        }
+    }
+
+    // User Input
+    public class TextField(string name, ShPlayer player, string callBackEvent) : BaseField(name, player)
+    {
+        public string Text { get; set; }
+
+        public override Promise GetValue()
+        {
+            Player.svPlayer.GetTextFieldText(Name, callBackEvent);
+            return base.GetValue();
+        }
+
+        [Obsolete("This is a input field, it can't be updated due to BP missing methode")]
+        public void UpdateTo(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void _DataReceivedFromClient(string data)
+        {
+            if (Promise.CurState == PromiseState.Resolved)
+            {
+                UnityEngine.Debug.LogWarning($"{Name} was already resolved!");
+                if (Text != data)
+                {
+                    UnityEngine.Debug.LogError($"{Name} was already resolved, while diffrent value arrived!");
+                }
+                Text = data;
+            }
+            else
+            {
+                Text = data;
+                Promise.Resolve();
+            }
+        }
+    }
+
+    // Label
+    public class TextElement(string name, ShPlayer player, string callBackEvent) : BaseField(name, player)
+    {
+        public string Text { get; set; }
+
+        [Obsolete("This is a label, it can't be get Valued due to BP missing methode")]
+        public override Promise GetValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTo(string text)
+        {
+            Player.svPlayer.SetTextElementText(Name, text);
+        }
+
+        public void _DataReceivedFromClient(string data)
+        {
+            if (Promise.CurState == PromiseState.Resolved)
+            {
+                UnityEngine.Debug.LogWarning($"{Name} was already resolved!");
+                if (Text != data)
+                {
+                    UnityEngine.Debug.LogError($"{Name} was already resolved, while diffrent value arrived!");
+                }
+                Text = data;
+            }
+            else
+            {
+                Text = data;
+                Promise.Resolve();
+            }
+        }
+    }
+
+    public class CheckboxField(string name, ShPlayer player, string callBackEvent) : BaseField(name, player)
+    {
+        public bool CheckboxValue { get; internal set; }
+
+        public override Promise GetValue()
+        {
+            player.svPlayer.GetToggleValue(Name, callBackEvent);
+            return base.GetValue();
+        }
+
+        public void UpdateTo(bool value)
+        {
+            Player.svPlayer.SetToggleValue(Name, value);
+        }
+
+        public void _DataReceivedFromClient(bool data)
+        {
+            if (Promise.CurState == RSG.PromiseState.Resolved)
+            {
+                UnityEngine.Debug.LogWarning($"{Name} was already resolved!");
+                if (CheckboxValue != data)
+                {
+                    UnityEngine.Debug.LogError($"{Name} was already resolved, while diffrent value arrived!");
+                }
+                CheckboxValue = data;
+            }
+            else
+            {
+                CheckboxValue = data;
+                Promise.Resolve();
+            }
+        }
+    }
+}
